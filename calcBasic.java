a3f85c4f99bf5c3af9949f78e3b3eff66db90812
bucandy <andybuchel@hotmail.com>
2016-07-10 19:32:19 +1000

    Final Build for Very Basic

--- calcBasic.java
+++ calcBasic.java
@@ -1,26 +1,69 @@
 import java.util.Scanner;
 
 public class calcBasic {
-	static int ans, firstNumber, secondNumber;
 	public static void main (String[] args){	
-		System.out.println("This is a Basic Calculator Program. With this program you can add, subtract, multipy or divide two numbers.");
+
+		// Define variables
+		float ans = 0, firstNumber = 0, secondNumber = 0;
+		boolean operatorCheck = false;
+		String userOperator = null;
+		
+		// Introduction
+		System.out.println("This is a Basic Calculator Program.");
+		System.out.println("With this program you can add, subtract, multipy or divide two numbers.");
 		System.out.println();
-		System.out.println("Please enter 1st Number");
+		
+		// Input first number
 		Scanner userInput1 = new Scanner (System.in);
-		firstNumber = userInput1.nextInt();
+		System.out.println("Please enter 1st Number");
+		while (!userInput1.hasNextFloat()){
+			System.out.println("Error: Please enter number.");
+			userInput1.next();
+		}
+		firstNumber = userInput1.nextFloat();
 		System.out.println();
+		
+		// Input operator
 		System.out.println("Please type an operator:");
 		System.out.println("'+' for addition");
 		System.out.println("'-' for subtraction");
 		System.out.println("'*' for multiplication");
 		System.out.println("'/' for divison");
 		Scanner userInput2 = new Scanner (System.in);	
-		String userOP = userInput2.nextLine();
+		while (operatorCheck==false){
+			userOperator = userInput2.nextLine();
+			switch (userOperator){
+				case "+": 
+					System.out.println("You Chose " + userOperator);
+					operatorCheck = true;
+					break;
+				case "-": operatorCheck = true;
+				System.out.println("You Chose " + userOperator);
+					break;
+				case "/": operatorCheck = true;
+				System.out.println("You Chose " + userOperator);
+					break;
+				case "*": operatorCheck = true;
+				System.out.println("You Chose " + userOperator);
+					break;
+				default: System.out.println("ERROR: Please enter valid operator.");
+					break;
+			}
+		}
 		System.out.println();
+		
+		// Input second number
 		System.out.println("Please enter 2nd Number");
 		Scanner userInput3 = new Scanner (System.in);	
-		secondNumber = userInput3.nextInt();
-		switch(userOP) {
+		while (!userInput3.hasNextFloat()){
+			System.out.println("Error: Please enter number.");
+			userInput3.next();
+		}
+		secondNumber = userInput3.nextFloat();
+		System.out.println();
+		
+		// Perform and output calculation
+		switch(userOperator) {
 			case "+": ans = firstNumber+secondNumber;
 				break;
 			case "-": ans = firstNumber-secondNumber;
@@ -30,7 +73,10 @@ public class calcBasic {
 			case "/": ans = firstNumber/secondNumber;
 				break;
 		}
-		System.out.println();
-		System.out.println("The answer is "+ans);
+		System.out.println(firstNumber + " " + userOperator + " " + secondNumber + " = " + ans);
+		userInput1.close();
+		userInput2.close();
+		userInput3.close();
+	
 	}
 }
